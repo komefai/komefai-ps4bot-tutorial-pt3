eccd9bc03306a0a824e7aefc86df760af1b9bf73
Komefai <komefai@users.noreply.github.com>
2017-09-18 22:23:07 +0000

    first commit

---
+++ ExampleScript/InteractionMenu.cs
@@ -0,0 +1,42 @@
+﻿using PS4MacroAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExampleScript
+{
+    class InteractionMenu : Scene
+    {
+        public override string Name => "Interaction Menu";
+
+        public static RectMap HeaderBar = new RectMap()
+        {
+            X = 52,
+            Y = 111,
+            Width = 224,
+            Height = 73,
+            Hash = 9223371914439753728
+        };
+
+        public static RectMap QuickGPS = new RectMap()
+        {
+            X = 51,
+            Y = 186,
+            Width = 137,
+            Height = 19,
+            Hash = 6953398460151201663
+        };
+
+        public override bool Match(ScriptBase script)
+        {
+            return script.MatchTemplate(HeaderBar, 95) && script.MatchTemplate(QuickGPS, 92);
+        }
+
+        public override void OnMatched(ScriptBase script)
+        {
+            script.Press(new DualShockState() { DPad_Right = true });
+            script.Press(new DualShockState() { Cross = true });
+        }
+    }
+}
---
+++ ExampleScript/Script.cs
@@ -0,0 +1,34 @@
+﻿using PS4MacroAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExampleScript
+{
+    public class Script : ScriptBase
+    {
+        /* Constructor */
+        public Script()
+        {
+            Config.Name = "Example Script";
+            Config.Scenes = new List<Scene>()
+            {
+                new InteractionMenu()
+            };
+        }
+
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void Update()
+        {
+            HandleScenes(scene =>
+            {
+                Console.WriteLine(scene.Name);
+            });
+        }
+    }
+}
